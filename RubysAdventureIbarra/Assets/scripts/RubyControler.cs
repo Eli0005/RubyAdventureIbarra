3930fd45184f65d2e78c57293e7addb3069fa2be
Eli0005 <714190@student.dusd.net>
2022-12-02 09:54:16 -0800

    adding new scripts to Ruby

--- RubysAdventureIbarra/Assets/scripts/RubyControler.cs
+++ RubysAdventureIbarra/Assets/scripts/RubyControler.cs
@@ -7,23 +7,17 @@ public class RubyControler : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Vector2 postion = transform.position;
-        position.x = position.x + 0.1f;
-        transform.position = position;
+        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        //create 2 varibales to use the unity built in axes
         float horizontal = Input.GetAxis("Horizontal");
-        float vertical = input.GetAxis("Vertical");
-
-        //Create our movement vector
-        vector2 position = transform.position;
-
-        //create horizontal and vertical movement
-        position.x = position.x + 3.0f * horizontal * Time.delta;
-        position.y = position.y + 3.0f * vertical * Time.delta;
+        Debug.Log(horizontal);
+        Vector2 postion = transform.position;
+        position.x = position.x + 0.1f;
+        transform.position = position;
     }
 }
