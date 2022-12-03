dd8fddb63e1a652f48854f79b1d6198f5238a705
Eli0005 <714190@student.dusd.net>
2022-12-03 12:44:28 -0800

    finishing ruby's movement

--- RubysAdventureIbarra/Assets/scripts/RubyControler.cs
+++ RubysAdventureIbarra/Assets/scripts/RubyControler.cs
@@ -7,17 +7,20 @@ public class RubyControler : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
-
+        //QualitySettings.vSyncCount = 0;
+        //Application.targetFrameRate = 10;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        Debug.Log(horizontal);
-        Vector2 postion = transform.position;
-        position.x = position.x + 0.1f;
+        float movement = 3.0f;
+        float horizontal = Input .GetAxis("Horizontal");
+        float vertical = Input .GetAxis("Vertical");
+        
+        Vector2 position = transform.position;
+        position.x = position.x + movement * horizontal * Time.deltaTime;
+        position.y = position.y + movement * vertical * Time.deltaTime;
         transform.position = position;
     }
 }
